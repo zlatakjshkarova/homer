60324630d952e73e5af46ae4f7f5bf025e6d4805
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-11-08 22:03:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let a = 1;
+while (a < 10) {
+    console.log(a);
+    a++
+}
+
+for (let b = 1; b < 10; b++) {
+    console.log(b);
+}
+for (let cc = 0; cc < 20; ++cc) {
+    if (cc % 20 == 0) continue
+    console.log(++cc)
+}
+
+
+let ae = 2;
+let pass;
+while (ae < 3) {
+    pass = prompt('человека имя, год рождения, место жительства и формирует следующее ')
+    if (pass == 5678908765) {
+        console.log('Добрый день Владимир, вы проживаете в городе Тюмень. Ваш возраст – 30 лет»');
+        break;
+    } else {
+        console.log('no');
+    }
+}
+
+
+for (let y = 0; y < 2000; y++) {
+    if (y % 5 == 0) continue;
+    console.log(h)
+}
+
+for (let ke = 0; ke < 5000; ke++) {
+    if (ke % 10 == 0) continue;
+    console.log(ke)
+}
+for (let ne = 0; ne < 10000; ne++) {
+    if (ne % 5 == 0) continue;
+    console.log(ne)
+
+}
+
+
+
+
+
+
+
+
+
